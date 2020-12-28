c646abf99bd7e4bc5717aeeaed44c8f8aca831f6
Steve Marquez <steveantonio00@gmail.com>
2020-12-28 12:05:20 -0800

    Finish first introduction of eloquentJS

---
+++ chapter1.js
@@ -0,0 +1,4 @@
+/*
+VALUES, TYPES AND OPERATORS
+
+*/
---
+++ introduction.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+/*
+Programming is the act of constructing a program—a set of precise instructions telling a computer what to do.
+
+A programming language is an artificially constructed language used to instruct computers.
+*/
+
+/*
+WHY LAGUANGE MATTERS?
+Code is the text that makes up programs.
+
+*/
+
+/*
+WHY LAGUANGE MATTERS?
+Code is the text that makes up programs.
+
+*/
