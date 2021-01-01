808add4a67f313571d636b5832389b2b129fdd5a
Steve Marquez <steveantonio00@gmail.com>
2021-01-01 12:08:54 -0800

    Finish chapter 1

--- chapter1.js
+++ chapter1.js
@@ -1,4 +1,55 @@
+/* eslint-disable max-len */
 /*
 VALUES, TYPES AND OPERATORS
 
 */
+
+// All data stored in a computer are sequences of bits.
+
+// Bits are two-valued things. They are represented with 1 and 0 . Think of it as black and white or true and false.
+// Any piece of information can be esessially zeros and ones represented in Bits.
+// LEARN HOW TO CALCULATE BINARY NUMBERS
+
+// VALUES //
+
+// In javascript these are pieces of information that we call values
+// Every value has its type that determines its responsability and duties.
+// As long as you no longer use a value be desappear, but it will be in memory if you need to call it again.
+
+// NUMBERS
+
+// Javascript uses 64 bits.
+// 64 bits are like 18 quintillion so you can represent your number and be able to store it in memory.
+// Numbers and calculations for whole numbers or integers smaller than 9 quadrillion wil be precise.
+
+// ArithMetic
+// Pendas in math apply here.
+// Modulo is the remainder of a division X % Y = (X / Y) = remainder
+
+// Strings
+//  backslash \ is 'escaping' character
+
+// Javascript models strings into a sequence of bits.
+// This is based on the Unicode standards.
+// putthing two strings together 'a' + 'b' concatenates the strings = 'ab'
+
+// Backticks- quotes strings are called 'template literals'
+
+// If we use two value operators these are called ' Binary Operators' example - x > y, x + y
+// If we use one value operator this one is called ' Unary Operator' example !, NaN
+// if we use three value operator, this one is called ' Ternary Operator ' console.log(true ? 1 : 2)
+
+// console.log(NaN == NaN) // False
+//  Ternary Operator
+// console.log(true ? 1 : 2) // 1
+// console.log(false ? 1 : 2) // 2
+
+// Empty Values
+
+// null and undefined  - indicates that the variable in memory doesnt have a meaninful value yet
+// They are interchangable
+// example :
+// let empty;
+// console.log(empty); // undefined
+
+// O, NaN and "" count as false
