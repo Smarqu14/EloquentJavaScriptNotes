37d0bae703650666ef70c67994d6907c95c476a6
Steve Marquez <steveantonio00@gmail.com>
2021-01-02 16:07:13 -0800

    Finish chapter two

---
+++ chapter2.js
@@ -0,0 +1,42 @@
+// PROGRAM STRUCTURE
+/*
+
+//  Expressions and Statements
+
+    // - an Expression: a piece of code that has a result is called 
+    //   - 22 or "psuchopath" are expressions.
+    //   - An expression between () is also an expression
+    //   - Expressions can contain other expresisons   
+
+
+//  Bindings
+// JavaScript produces binding or variable 
+  let name = 'Steve';
+
+
+
+
+
+  // The Environment  
+    // - The list of bindings (variables) and their value that exist is the environment.
+
+
+    // Functions
+    A function is a piece of program wrapped in a value.
+
+    - Executing a function is invoking, calling or applying it.
+    - Values that you pu
+
+
+    // Return Values
+
+    showing a dialog box or writing text to the screen is a side effect.
+
+    returns the max of any arguments - Math.max(1,2,3,4,6,1999)
+
+
+
+
+
+
+/*
