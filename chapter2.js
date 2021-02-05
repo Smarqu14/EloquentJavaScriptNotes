ed8d7108d9ab726c53d891f290f946b66b00c381
Steve Marquez <steveantonio00@gmail.com>
2021-02-05 11:34:08 -0800

    Create chapter3 file

--- chapter2.js
+++ chapter2.js
@@ -27,12 +27,20 @@
     - Executing a function is invoking, calling or applying it.
     - Values that you pu
 
-
+o
     // Return Values
 
     showing a dialog box or writing text to the screen is a side effect.
 
     returns the max of any arguments - Math.max(1,2,3,4,6,1999)
+   it does the same thing - Math.min(1,2,3,-5)
+
+
+
+   //Control Flow
+    Statements are executed as a story from top to bottom
+
+     Number.isNaN - function is a standard JavaScript function that returns true only if the arguments it is given is NaN.
 
 
 
